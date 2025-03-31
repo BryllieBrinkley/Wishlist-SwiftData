0a751468743fafa15b10fa9a92fea908ad843861
Jibryll Brinkley <jibryllbrink@yahoo.com>
2025-03-30 21:42:26 -0400

    Initial Commit

---
+++ Wishlist/WishlistApp.swift
@@ -0,0 +1,17 @@
+//
+//  WishlistApp.swift
+//  Wishlist
+//
+//  Created by Jibryll Brinkley on 3/30/25.
+//
+
+import SwiftUI
+
+@main
+struct WishlistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
