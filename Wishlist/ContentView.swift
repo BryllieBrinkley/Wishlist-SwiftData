fee13517e66bb135dd13c44877bfd4353f8e54c9
Jibryll Brinkley <jibryllbrink@yahoo.com>
2025-03-30 23:02:07 -0400

    Sample data created; UI enhanced

--- Wishlist/ContentView.swift
+++ Wishlist/ContentView.swift
@@ -17,6 +17,8 @@ struct ContentView: View {
             List {
                 ForEach(wishes) { wish in
                     Text(wish.title)
+                        .font(.title.weight(.light))
+                        .padding(.vertical, 2)
                 }
             }
             .navigationTitle("Wishlist")
@@ -29,7 +31,22 @@ struct ContentView: View {
     }
 }
 
-#Preview {
+#Preview("List with Sample Data") {
+    let container = try! ModelContainer(for: Wish.self, configurations:
+                                            ModelConfiguration(isStoredInMemoryOnly: true))
+    
+    container.mainContext.insert(Wish(title: "Buy a New Macbook"))
+    container.mainContext.insert(Wish(title: "Download Xcode"))
+    container.mainContext.insert(Wish(title: "Learn new technologies"))
+    container.mainContext.insert(Wish(title: "Publish App"))
+    container.mainContext.insert(Wish(title: "Travel to Europe"))
+    
+    return ContentView()
+        .modelContainer(container)
+}
+
+#Preview("Empty List") {
     ContentView()
         .modelContainer(for: Wish.self, inMemory: true)
 }
+
